db2470d9246e00e8417a6ab9d7b241a25418e385
ashley <ashleyaarndt@outlook.com>
2019-04-21 18:14:08 -0400

    final

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+CREATE 
+DATABASE burger_db;
+
+USE burger_db;
+
+
+
+
+
+CREATE TABLE burgers
+
+(
+
+    id int
+        NOT NULL
+    AUTO_INCREMENT,
+
+    burger_name varchar
+    (500)
+NOT 
+NULL,
+
+devoured boolean 
+NOT NULL,
+
+    PRIMARY 
+KEY
+    (id)
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+
+INSERT 
+INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Tunami", true);
+
+
+
+INSERT 
+INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Fig-Eta Bout It ", false);
+
+
+
+INSERT 
+INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Yes I Cayanne", true);
